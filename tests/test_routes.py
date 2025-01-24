68817f97e47742b74ce7954458adf50a9272bf9b
tooba <tooba_babar@outlook.com>
2025-01-24 11:48:01 -0500

    made changes t routes.py file

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,18 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read an Account"""
+        account = self._create_accounts(0)[1]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}",
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_account_not_account(self):
+        """It should look for an Account that does not exist"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
